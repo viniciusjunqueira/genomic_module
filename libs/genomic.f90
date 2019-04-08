17935488d38d39ea77573646a39ac1bbb105a9d9
Vinicius Silva Junqueira <junqueiravinicius@hotmail.com>
2019-04-08 00:06:43 -0300

    creating and modifying genomic codes

--- libs/genomic.f90
+++ libs/genomic.f90
@@ -16,16 +16,35 @@ use kinds; use model; use textop; use sparsem
 use sparseop; use denseop; use prob; use pcg
 implicit none
 ! 
-character :: version*5= "1.108"
+character :: version*5= "1.000"
 real(r8) ::  wGimA22i =  1.d0,&     
              wGi      =  1.d0,&    
              wA22i    = -1.d0      
 logical :: sameweights
-integer :: whichH=0             
+integer :: whichH=0
+
 !
 ! storage matrices
 !
 type(densem),save :: A22i,Gi,GimA22i
+
+
+integer ::  io_g=10, io_i, io, i, j, l, begSNP, lastSNP, len_gen
+integer :: num_snp=80000, len_char=50
+integer :: unit_xref = 123
+integer, allocatable :: xref(:)
+
+real (rh) :: temp(1),t1,t2,val
+
+real (rh), allocatable :: gen(:,:), W(:,:), Z(:,:), k(:)
+
+character (len=100) :: a, gen_file,&! = "genotypes.gen",&
+                       g_xref,& !="genotypes.gen_XrefID",&
+                       ped_file! = "pedigree.ped"
+
+
+
+
 
 
 !=============================================================
@@ -49,11 +68,11 @@ interface createGmA22
                   createGmA22_dense_symm ,&
                   createGmA22_sparse_hashm
 end interface
+
 contains
 !==================================================================
 ! Subroutines & functions
 !==================================================================
-  
 subroutine setup_genomic(eff, unit_eff)
    ! 
    ! Create/Store/Read G matrix, A22 matrix and GmA22 matrix and its inverses
@@ -68,26 +87,176 @@ subroutine setup_genomic(eff, unit_eff)
    print '(a,i3,a)' ,' *  Modified relationship matrix (H) created for effect: ',eff,'    *'
    print '(a)'      ,' *--------------------------------------------------------------*'     
    print*,''
-   print*,          '  * Not available in this distribution'
-  stop   
+   !print*,          '  * Not available in this distribution'
+   !stop
+
+   gen_file = "genotypes.gen"
+   g_xref = "genotypes.gen_XrefID"
+   !ped_file = "pedigree.ped"
+
+   open(unit_xref, file=g_xref)
+   open(io_g, file=gen_file)
+
+   call read_xref_file(len_gen)
+   call read_first_and_last_snp(len_gen,begsnp,lastsnp)
+
+   print '(a35,i10)','Number of genotyped individuals = ',len_gen
+   print '(a35,i10)','Position of first SNP = ',begsnp
+   print '(a35,i10)','Number of genotypes = ',lastsnp
+   print*,''
+   
+   !allocate( lenped(1) )
+   !neff=1
+   !lenped(neff)=4
+   allocate( gen(len_gen,lastsnp) )
+   call create_Gi_dense_symm(gen,len_gen,begsnp,lastsnp)
+   
+
+end subroutine
+
+
+subroutine create_Gi_dense_symm(x,n,pos1,pos2)
+   implicit none
+   real(rh) :: x(:,:)
+   integer :: n,pos1,pos2
+   integer  :: i,j,l
+   real (rh) :: val
+   integer :: io_save=30
+
+   allocate( w(3,pos2), Z(n,pos2), k(pos2) )
+   
+   call zerom(Gi,n)
+   
+   call read_markers(x, n, pos1, pos2)
+
+   call createk(x,n,k)
+   call createW(x,W,n,pos2)
+
+   open(io_save,file="G",status='replace')
+   print '(a)', 'Creating genomic matrix...'
+   call cpu_time(t1)
+   Z=0
+   do l=1,pos2
+      Z(:,l)=W(x(:,l)+1,l) ! centering column by gene content
+   end do
+   do i=1,n
+      do j=1,n
+         do l=1,pos2
+            val = getm(i,j,Gi) + Z(i,l)*Z(j,l)
+            call setm(val,i,j,Gi)
+         end do
+      end do
+   end do
+   do i=1,n
+      do j=1,n
+         val = getm(i,j,Gi)/sqrt(k(i)*k(j))
+          call setm(val,i,j,Gi)
+         if (i<=j) write(io_save,fmt="(2i,f11.3)") i,j, getm(i,j,Gi)
+      end do
+   end do
+
+   ! ====================================================================
+   call cpu_time(t2)
+
+   ! scaling
+   !print*, 'Scaling G matrix'
+   !do i=1,n
+   !    do j=1,n
+   !        !G(i,j) = G(i,j) * .95
+   !        val = getm(i,j,Gi) * .95
+   !        call setm(val,i,j,Gi)
+   !        !if (i==j) G(i,j) = G(i,j) + .05
+   !        if (i==j) then
+   !         val = getm(i,j,Gi) + .05
+   !         call setm(val,i,j,Gi)
+   !        endif
+   !    enddo
+   !enddo
+   print '(a35,f10.2)', 'Time to construct G = ',t2-t1
+   if(n <= 10) call printm(Gi)
+   close(io_save)
+   
+
+end subroutine
+
+! Function that compute variance of a vector
+function var(x) result(c)
+implicit none
+real (rh) :: x(:),c,mu
+integer :: i
+
+! Mean
+mu=sum(x)/size(x)
+! Sum of Squares
+do i=1,size(x)
+  c=c+(x(i)-mu)**2
+end do 
+c=c/size(x)
+end function
+
+
+real function freq(values,n) result(x)
+integer :: i,j,n
+real (rh) :: f,values(:),np,npq,nmiss=0
+np = count(values==0.)
+npq = count(values==1.)
+nmiss = count(values==5.)
+x = (np + npq*.5)/(n-nmiss)
+end function
+
+
+! Subroutine to create a vector of 2pq
+subroutine createk(m,n,k)
+implicit none
+real (rh) :: m(:,:), k(:), p, q, pq, val
+integer :: i,j,n,io_save=20
+
+!open(io_save,file="allele_freq")
+print '(a)', 'Creating scaling (k) vector'
+call cpu_time(t1)
+do i=1,n
+    p=0;val=0
+    do j=1,size(m,2)
+        p=freq(m(:,j),size(m,1))
+        q=1-p
+        !write(io_save,fmt="(5f15.3)"), p,q,p*q,2*p*q
+        val=val+2*p*q
+    enddo
+    k(i) = val
+enddo
+call cpu_time(t2)
+print '(a35,f10.2)', 'Time to compute k  = ',t2-t1
+close(io_save)
+end subroutine
+
+
+subroutine createW(m,W,n,snp)
+implicit none
+real (rh) :: m(:,:), W(:,:), val
+integer :: n,snp,i
+
+do i=1,snp
+    val = freq(m(:,i),n)
+    W(1,i)=-2*val; W(2,i)=1-2*val; W(3,i)=2-2*val
+enddo
 end subroutine
 
 subroutine defaults_and_checks_GS()
    ! check optional parameters for Genomic Selection
-   integer::n
+   integer :: n
    character (100):: xc(50)
    real :: x(50)
    print*,''
-   do
-      call getoption('SNP_file',n,x,xc)
-      if (n > 0) then ! if SNP_file is present 
+   !do
+      !call getoption('SNP_file',n,x,xc)
+      !if (n > 0) then ! if SNP_file is present 
             call setup_genomic(0,0) 
-            stop
-      endif ! if SNP_file present
-      
-      call getoption('end',n,x,xc)
-      if(n <= 0) exit
-   enddo
+      !      stop
+      !endif ! if SNP_file present
+      !
+      !call getoption('end',n,x,xc)
+      !if(n <= 0) exit
+   !enddo
 end subroutine
 
 
@@ -180,6 +349,7 @@ subroutine createGmA22_dense_symm(X,Y,XmY,w2,wx2,wy2)
    real :: t1
    !
 endsubroutine
+
 subroutine createGmA22_densem(X,Y,XmY,w2,wx2,wy2)
    ! creates Matrix Adelta (wx*X + wy*Y)*w in densem format
    !
@@ -201,5 +371,87 @@ subroutine createGmA22_sparse_hashm(X,Y,XmY,w2,wx2,wy2)
    real :: t1
    !
 endsubroutine
+
+! Subroutine that computes the number of markers and samples
+subroutine read_first_and_last_snp(n,beg,bot)
+implicit none
+character(len=num_snp) :: line
+integer :: n, beg, bot, curr_pos
+
+print*,n
+do i=1, n
+  read(io_g, fmt='(a)', iostat=io) line
+  if (i == 1) then
+    beg = extract_snp(line, index( line(1:1000), " ")+1)
+    bot = len(trim(line(beg:)))
+  else
+    curr_pos = extract_snp(line, index( line(1:1000), " ")+1)
+    if (curr_pos /= beg) then
+        print*, 'Position of first SNP is different! Check genotypic file!'
+        print '( 3(a,i5) )', 'Position level     1 = ', beg, ' / Position level ',i,' = ', curr_pos
+        stop
+    endif
+  endif
+end do
+rewind io_g
+end subroutine read_first_and_last_snp
+
+
+integer function extract_snp(x,curPos) result(updatePOS)
+character(len_char) :: x
+integer :: curPos
+do
+    if( x(curPos:curPos) /= " ") exit
+    curPos = curPos+1
+enddo
+updatePOS = curPos
+end function
+
+! Read cross reference file
+subroutine read_xref_file(n)
+integer :: i,io,n,tmp(2)
+xref=0
+
+n=0
+do
+  read(unit_xref,*,iostat=io) i
+  if( io .ne. 0) exit
+  n=n+1
+enddo
+rewind unit_xref
+allocate(xref(n))
+do i=1,n
+   read(unit_xref,*,iostat=io) xref(i)
+enddo
+
+end subroutine
+
+! Subroutine that reads genotype file as character and converts it into real
+subroutine read_markers(x,n,first_snp,nsnp)
+implicit none
+
+real (rh) :: x(:,:)
+integer :: n,i,j,pos,snp,first_snp,nsnp
+character(len=num_snp) :: line
+character(len=1) :: a
+
+print '(a)', 'Reading marker data...'
+call cpu_time(t1)
+do i=1,n
+    !if(mod(i,1000)==0) print*,i
+    pos=first_snp
+    read(io_g,fmt='(a)') line
+    do j=1,nsnp    
+        a=line(pos:pos)
+        read(a,'(i)') snp
+        gen(i,j)=snp
+        pos=pos+1
+    end do
+enddo
+call cpu_time(t2)
+print '(a35,f10.2)', 'Time to read genotypic data = ',t2-t1
+end subroutine
+
+
 
 end module
